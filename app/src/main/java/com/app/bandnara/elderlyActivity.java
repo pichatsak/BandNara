ce6ff05f2dfda8d72f6f59ff00ea6fbedb31de37
Jui <sookma28@gmail.com>
2022-05-05 01:01:21 +0700

    เพิ่มหน้าขึ้นทะเบียนผู้สูงอายุ

--- app/src/main/java/com/app/bandnara/elderlyActivity.java
+++ app/src/main/java/com/app/bandnara/elderlyActivity.java
@@ -2,7 +2,12 @@ package com.app.bandnara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.app.bandnara.ToolBar.CloseBar;
 
@@ -13,5 +18,25 @@ public class elderlyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elderly);
         CloseBar closeBar = new CloseBar(this);
+
+
+
+        final Dialog dialog = new Dialog(elderlyActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.dialog_hightold);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+//        dialog.show();
+//        Display display =((WindowManager)getSystemService(elderlyActivity.this.WINDOW_SERVICE)).getDefaultDisplay();
+//        int width = display.getWidth();
+//        int height=display.getHeight();
+//        Log.v("width", width+"");
+//        dialog.getWindow().setLayout((6*width)/7,(4*height)/3);
+
+        int width = (int)(getResources().getDisplayMetrics().widthPixels*0.90);
+        int height = (int)(getResources().getDisplayMetrics().heightPixels*0.90);
+
+        dialog.getWindow().setLayout(width, height);
     }
 }
